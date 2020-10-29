57e61d5258e0447165e532c6d7837006b640bc82
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-10-28 21:27:00 -0300

    Create Helper.ino

---
+++ AlimentadorESP32/Helper.ino
@@ -0,0 +1,25 @@
+unsigned long tempoAtivacao = 0;
+
+bool temporizador(int milissegundos) {
+  unsigned long tempoAtual = millis();
+  if (tempoAtivacao == 0) {
+    tempoAtivacao = tempoAtual;
+  }
+  if (tempoAtual - tempoAtivacao > milissegundos)
+  {
+    tempoAtivacao = 0;
+    return false;
+  }
+  return true;
+}
+
+String preencherZeros(int numero, int tamanho) {
+  String textoNumero = String(numero);
+  int tamanhoNumero = textoNumero.length();
+  if (tamanhoNumero < tamanho) {
+    for (int n = 0; n < (tamanho - tamanhoNumero); n++) {
+      textoNumero = "0" + textoNumero;
+    }
+  }
+  return textoNumero;
+}
