99d5e04d85caae880a06ae0cb9666213d69d6fa1
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-10-29 19:53:48 -0300

    Update Buzzer.ino

--- AlimentadorESP32/Buzzer.ino
+++ AlimentadorESP32/Buzzer.ino
@@ -1,12 +1,26 @@
-int totalNotas = 200;
-int NotasMusica[] = {};
-int duracaoNotas[] = {};
-
+int alertaSelecionado = 0;
 int notaAtual = 0;
 bool pararSom = false;
 
 void iniciarBuzzer() {
   EasyBuzzer.setPin(PINO_BUZZER);
+}
+
+void selecionarAlerta(int numeroAlerta) {
+  alertaSelecionado = numeroAlerta;
+}
+
+int* obterAlerta() {
+  switch (alertaSelecionado) {
+    case 0:
+      return melodyNokia;
+    case 1:
+      return melodyStarTrek;
+    case 2:
+      return melodyJigglypuff;
+    default:
+      return melodyNokia;
+  }
 }
 
 void pararMusica() {
@@ -15,6 +29,8 @@ void pararMusica() {
 }
 
 void tocarNota() {
+  int* musicaAtual = obterAlerta();
+  int totalNotas = sizeof(musicaAtual) / sizeof(musicaAtual[0]) / 2;
   if (pararSom == true) {
     return;
   }
@@ -22,11 +38,28 @@ void tocarNota() {
     notaAtual = 0;
   }
   EasyBuzzer.singleBeep(
-    NotasMusica[notaAtual],
-    duracaoNotas[notaAtual],
+    musicaAtual[notaAtual],
+    obterTempo(musicaAtual[notaAtual+1]),
     tocarNota
   );
-  notaAtual += 1;
+  notaAtual += 2;
+}
+
+
+int obterTempo(int divisor) {
+  int duracao = 0;
+  int compasso = (60000 * 4) / 80;
+  
+  if (divisor > 0) {
+    // regular note, just proceed
+    duracao = (compasso) / divisor;
+  } else if (divisor < 0) {
+    // dotted notes are represented with negative durations!!
+    duracao = (compasso) / abs(divisor);
+    duracao *= 1.5; // increases the duration in half for dotted notes
+  }
+
+  return duracao;
 }
 
 void atualizarMusica() {
