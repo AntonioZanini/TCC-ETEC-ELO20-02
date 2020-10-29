60dd71187ad02eb45c311bf2994a281628b04c60
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-10-28 21:27:07 -0300

    Create Buzzer.ino

---
+++ AlimentadorESP32/Buzzer.ino
@@ -0,0 +1,34 @@
+int totalNotas = 200;
+int NotasMusica[] = {};
+int duracaoNotas[] = {};
+
+int notaAtual = 0;
+bool pararSom = false;
+
+void iniciarBuzzer() {
+  EasyBuzzer.setPin(pinBuzzer);
+}
+
+void pararMusica() {
+  EasyBuzzer.stopBeep();
+  pararSom = true;
+}
+
+void tocarNota() {
+  if (pararSom == true) {
+    return;
+  }
+  if (notaAtual == totalNotas) {
+    notaAtual = 0;
+  }
+  EasyBuzzer.singleBeep(
+    NotasMusica[notaAtual],
+    duracaoNotas[notaAtual],
+    tocarNota
+  );
+  notaAtual += 1;
+}
+
+void atualizarMusica() {
+  EasyBuzzer.update();
+}
