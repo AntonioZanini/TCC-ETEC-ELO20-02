29b651ad4a737d0f160385cd7973b7d5fd8ab3a4
Antonio Zanini de Farias <antoniozfarias@hotmail.com>
2020-10-29 21:30:40 -0300

    Update WiFi.ino

--- AlimentadorESP32/WiFi.ino
+++ AlimentadorESP32/WiFi.ino
@@ -10,7 +10,9 @@ void conectarWifi() {
   while (WiFi.status() != WL_CONNECTED && temporizar(2000) == true)
   {
     delay(200);
-    Serial.println("Conectando ao WiFi...");
+      limparDisplay();
+      escreverDisplay("CONECTANDO", 0, 0);
+      escreverDisplay("A WI-FI...", 1, 0);
   } 
   if (WiFi.status() != WL_CONNECTED) {
     notificarFaltaWifi();
@@ -26,5 +28,7 @@ void verificarWifi() {
 }
 
 void notificarFaltaWifi() {
-  Serial.println("Falta Wi-Fi");
+  limparDisplay();
+  escreverDisplay("NAO FOI POSSIVEL", 0, 0);
+  escreverDisplay("CONECTAR A WI-FI", 1, 0);
 }
